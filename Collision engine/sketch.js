75788f9e15bf114671e8f2e5b04a3e82b1ca6897
Abu Dujana <78955466+Plagueowl@users.noreply.github.com>
2024-12-05 23:40:53 +0530

    Infuriating gravity effect

--- Collision engine/sketch.js
+++ Collision engine/sketch.js
@@ -1,7 +1,8 @@
-let ball1;
+let ball1,ball2;
 function setup() {
   createCanvas(600,600);
-  ball1 = new particle(5,5,2,3,70);
+  ball1 = new particle(500,110,3,2,70,10);
+  ball2 = new particle(50,70,3,3,20,10);
   
 }
 
@@ -9,8 +10,9 @@ function draw() {
   background(0);
   
   ball1.show();
-
-  ball1.collide();
+  ball2.show();
+  ball2.collide(ball1);
+  ball1.collide(ball2);
 }
 
 
@@ -18,10 +20,11 @@ function draw() {
 class particle{
   
   //initiates position and velocity vector
-  constructor(x,y , velocityX, velocityY, r){
+  constructor(x,y , velocityX, velocityY, r,mass){
     this.position = createVector(x,y);
     this.radius = r;
     this.velocity = createVector(velocityX,velocityY);
+    this.mass = mass;
   }
   //draws a circle for this example
   
@@ -32,7 +35,7 @@ class particle{
 
 
   //collision engine, covers edges and particle itself
-  collide(){
+  collide(other){
     //check edges
     if (this.position.x > height - this.radius) {
       this.position.x = height - this.radius;
@@ -50,6 +53,35 @@ class particle{
       this.position.y = this.radius;
       this.velocity.y *= -1;
     }
+
+
+    let impactVector = p5.Vector.sub(this.position,other.position);
+    let distance = impactVector.mag();
+    if(distance< this.radius + other.radius){
+
+      //correct if particles intersect, else they stick and move.
+      let overlap = distance-(other.radius + this.radius);
+      let direction = impactVector.copy();
+      direction.setMag(overlap*0.5);
+      this.position.add(direction);
+      other.position.sub(direction);
+      distance = this.radius + other.radius;
+      impactVector.setMag(distance);
+    // final velocity formulas, denoted from conservation of momentum and kinetic energy
+
+      
+      let velocitySub = p5.Vector.sub(other.velocity, this.velocity);
+      let numerator = 2*other.mass * p5.Vector.dot(velocitySub,impactVector);
+      let denominator = (this.mass + other.mass) *distance*distance;
+      let copy = impactVector.copy();
+      this.velocity.add(p5.Vector.mult(copy,(numerator/denominator)));
+      // print("here");
+      copy = impactVector.copy();
+
+      numerator = -2*this.mass * p5.Vector.dot(velocitySub,impactVector);
+      other.velocity.add(p5.Vector.mult(copy,(numerator/denominator)));
+    }
+
   }
 
   
