bbc3e025dc44ca91ae00dfdfb3ab46b56058b88c
Abu Dujana <78955466+Plagueowl@users.noreply.github.com>
2025-01-29 11:52:30 +0530

    Simple pendulum logic. Waves yet to be made
    I will make the pendulum wave simulation based on this code.

---
+++ Pendulum/sketch.js
@@ -0,0 +1,55 @@
+//This is the code for simulating a pendulum. There is damping logic as well which I'm working on
+//I am trying to make the pendulum wave simulation, I will learn about frequency, the length adjustments, etc and then rewrite this code to make proper waves.
+
+let pendulums;
+let n;
+function setup() {
+  createCanvas(600, 600);
+  n = 15;
+  pendulums = new Array();
+  for (var i = 0;i< n;i++){
+    pendulums.push(new pendulum(300,150,300 - i*15,10,PI/2));
+  }
+}
+
+function draw() {
+  background(220);
+  for(let object of pendulums){
+    object.show();
+    object.update();
+    // if(frameCount % 5 == 0){
+    //   object.dampen();
+
+    // }
+  }
+}
+
+class pendulum{
+  constructor(x,y,l,radius,theta,gravity = 0.1, mass = 1){
+    this.position = createVector(x,y);
+    this.l = l;
+    this.radius = radius;
+    this.theta = theta;
+    this.gravity = gravity;
+    this.mass = mass;
+    this.angularAcceleration = 0;
+    this.angularVelocity = 0;
+  }
+
+  show(){
+    line(this.position.x,this.position.y,this.position.x + this.l * sin(this.theta), this.position.y + this.l * cos(this.theta));
+
+    circle(this.position.x + this.l * sin(this.theta), this.position.y + this.l * cos(this.theta), this.radius);
+  }
+
+  update(){
+    //calculate the angular acceleration ( gravity * sin(theta) )
+    this.angularAcceleration = -this.gravity * sin(this.theta)/this.l;
+    this.angularVelocity += this.angularAcceleration;
+    this.theta += this.angularVelocity;
+  }
+  dampen(){
+    this.theta *= 0.99;
+  }
+
+}
