7ff87fc7f38268dc349f693d422cef43c9b52467
Abu Dujana <78955466+Plagueowl@users.noreply.github.com>
2025-02-10 11:44:59 +0530

    Much better and appealing version

--- Life using forces/sketch.js
+++ Life using forces/sketch.js
@@ -3,23 +3,23 @@ let arrayRed;
 let arrayBlue;
 let qtree;
 let checkedPairs = new Set();
-
-//will add comments later
-
 function setup() {
-  createCanvas(1000,1000);
-  
-  n = 200;
+  createCanvas(800,800);
+  // frameRate(10);
+  n = 400;
   arrayRed = new Array();
   arrayBlue = new Array();
   for(var i = 0;i< n;i++){
     arrayRed.push(new particle(random(height),random(width),5,1,i));
+  }
+  for(var i = 0;i< 1500;i++){
     arrayBlue.push(new particle(random(height),random(width),5,3,i));
+    
   }
 }
 
 function draw() {
-  background(255);
+  background(0);
   checkedPairs.clear();
 
   let boundary = new Rectangle(width/2,height/2,width,height);
@@ -36,24 +36,24 @@ function draw() {
     
     for(let neighbour of neighbours){
       if(neighbour.data.colour != particle.colour){
-        repel(neighbour.data, particle,0.08);
+        repel(neighbour.data, particle,5);
       }
       else{
-        attract(neighbour.data, particle,0.1);
+        attract(neighbour.data, particle,1);
       }
       
-      let currentNearbyParticle = neighbour.data;
+//       let currentNearbyParticle = neighbour.data;
       
-      //Making a simple encrypted string of pairs of particles and checking if they have been examined before. Here is where the ID assigned to the particle comes in use
-      if(currentNearbyParticle !== particle){
-        let idA = currentNearbyParticle.id;
-        let idB = particle.id;
-        let hashString = idA<idB ? `${idA}:${idB}` : `${idB}:${idA}`;
-        if(!checkedPairs.has(hashString)){
-          particle.collide(currentNearbyParticle);
-          checkedPairs.add(hashString);
-        }
-      }
+      // //Making a simple encrypted string of pairs of particles and checking if they have been examined before. Here is where the ID assigned to the particle comes in use
+      // if(currentNearbyParticle !== particle){
+      //   let idA = currentNearbyParticle.id;
+      //   let idB = particle.id;
+      //   let hashString = idA<idB ? `${idA}:${idB}` : `${idB}:${idA}`;
+      //   if(!checkedPairs.has(hashString)){
+      //     particle.collide(currentNearbyParticle);
+      //     checkedPairs.add(hashString);
+      //   }
+      // }
     
     }
   }
@@ -64,14 +64,15 @@ function draw() {
     let neighbours = qtree.detectPoints(range);
     for(let neighbour of neighbours){
       if(neighbour.data.colour != particle.colour){
-        attract(neighbour.data, particle,0.08);
+        repel(neighbour.data, particle,5);
       }
-      else{
-        repel(neighbour.data, particle,0.1);
-      }
+      // else{
+        // repel(neighbour.data, particle,1);
+      // }
     }
   }
   show();
+  // qtree.show();
   
 
   
@@ -79,18 +80,25 @@ function draw() {
 
 
 function attract(point1,point2, intensity){
-  let force = p5.Vector.sub(point1.position,point2.position);
-  point2.applyForce(force.setMag(intensity));
-  point1.applyForce(force.mult(-1));
+  let distance = p5.Vector.sub(point1.position,point2.position);
+  
+  if(distance.mag() > 0 && distance.mag() < 80){
+    let force = distance.copy().setMag(intensity/distance.mag());
+    point1.applyForce(force.mult(-0.5));
+    point2.applyForce(force.mult(0.5));
+  }
 }
 
 function repel(point1,point2, intensity){
-  let force = p5.Vector.sub(point1.position,point2.position);
-  // print(force.x);
-  point1.applyForce(force.setMag(intensity));
-  point2.applyForce(force.mult(-1));
-}
+  let distance = p5.Vector.sub(point1.position,point2.position);
+    
+  if(distance.mag() > 0 && distance.mag() < 80){
 
+    let force = distance.copy().setMag(intensity/distance.mag());
+    point1.applyForce(force.mult(0.5));
+    point2.applyForce(force.mult(-0.5));
+  }
+}
 function show(){
   for(let particle of arrayRed){
     particle.update();
